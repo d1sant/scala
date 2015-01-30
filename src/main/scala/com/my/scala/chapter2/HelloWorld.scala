ab609173b6f2f58527232ab8c0a448edb5b5632d
Dmitry Palaznik <dpalaznik@gmail.com>
2015-01-30 17:43:10 +0300

    HelloWorld: introduced new val with explicit type of String

--- src/main/scala/com/my/scala/chapter2/HelloWorld.scala
+++ src/main/scala/com/my/scala/chapter2/HelloWorld.scala
@@ -2,6 +2,9 @@ package com.my.scala.chapter2
 
 object HelloWorld {
   def main (args: Array[String]) {
-    println("Hello, world!")
+    val msg = "Hello, world!"
+    println(msg)
+    val msg2: java.lang.String = "Hello again, world!"
+    println(msg2)
   }
 }
