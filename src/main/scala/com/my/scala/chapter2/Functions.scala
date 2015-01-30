4999585649e78edb6b7a234df7742d8e38ee5cee
Dmitry Palaznik <dpalaznik@gmail.com>
2015-01-30 18:17:10 +0300

    Functions implementations with different approaches

---
+++ src/main/scala/com/my/scala/chapter2/Functions.scala
@@ -0,0 +1,22 @@
+package com.my.scala.chapter2
+
+object Functions {
+
+  def max(x: Int, y: Int): Int = {
+    if (x > y) x
+    else y
+  }
+
+  def max2(x: Int, y: Int) = {
+    if (x > y) x else y
+  }
+
+  def max3(x: Int, y: Int) = if (x > y) x else y
+
+  def main(args: Array[String]) {
+    println(max(1, 2))
+    println(max2(3, 5))
+    println(max3(8, 13))
+  }
+
+}
