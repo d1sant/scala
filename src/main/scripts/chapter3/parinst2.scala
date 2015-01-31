9b3e1ca4997718d06318c928da755c5cd94b3bc8
Dmitry Palaznik <dpalaznik@gmail.com>
2015-01-31 19:59:48 +0300

    Methods istead of operators in scala script

--- src/main/scripts/chapter3/parinst2.scala
+++ src/main/scripts/chapter3/parinst2.scala
@@ -2,5 +2,5 @@ val greetStrings = new Array[String](3)
 greetStrings(0)= "Hello"
 greetStrings(1) = ", "
 greetStrings(2) = "world!\n"
-for (i <- (-1 + 1).to(2)) // just to show that we can do it more sophisticated
+for (i <- ((-1).+(1)).to(2)) // just to show that we can do it more sophisticated cause scala doesn't have operators such "+", "-"
   print(greetStrings(i))
