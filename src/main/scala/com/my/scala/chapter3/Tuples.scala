8967d603d3a71c9fc1383408072e42d7d92665d9
Dmitry Palaznik <dpalaznik@gmail.com>
2015-02-26 15:01:32 +0300

    Tuples: more examples

--- src/main/scala/com/my/scala/chapter3/Tuples.scala
+++ src/main/scala/com/my/scala/chapter3/Tuples.scala
@@ -1,7 +1,12 @@
 package com.my.scala.chapter3
 
+/**
+ * The build in functionality allows to create tuples up to 22 values, but you can extend them for even more.
+ */
 object Tuples {
+
   def main(args: Array[String]) {
+
     val pair = (99, "Luftballons")
     println(pair._1)
     println(pair._2 + "\n")
@@ -9,6 +14,12 @@ object Tuples {
     val triple = (100.1, "Ballons", 8)
     println(triple._1)
     println(triple._2)
-    println(triple._3)
+    println(triple._3 + "\n")
+
+    val quartile = ('a', "the", 1, 4.1)
+    println(quartile._1)
+    println(quartile._2)
+    println(quartile._3)
+    println(quartile._4)
   }
 }
