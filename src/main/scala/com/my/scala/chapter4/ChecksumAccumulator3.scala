5509d73c2c2453878bc4f213e9bd6b4b647099dd
Dmitry Palaznik <dpalaznik@gmail.com>
2015-03-12 11:35:59 +0300

    Another way to write 'void' methods

--- src/main/scala/com/my/scala/chapter4/ChecksumAccumulator3.scala
+++ src/main/scala/com/my/scala/chapter4/ChecksumAccumulator3.scala
@@ -2,6 +2,6 @@ package com.my.scala.chapter4
 
 class ChecksumAccumulator3 {
   private var sum = 0
-  def add(b: Byte): Unit = sum += b
+  def add(b: Byte) { sum += b }
   def checksum(): Int = ~(sum & 0xFF) + 1
 }
