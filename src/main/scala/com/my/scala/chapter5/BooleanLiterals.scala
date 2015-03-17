3e1b6d9c6bb2b7704cb7d24930dd5c86e7252f0a
Dmitry Palaznik <dpalaznik@gmail.com>
2015-03-17 15:08:35 +0300

    Literals for booleans

---
+++ src/main/scala/com/my/scala/chapter5/BooleanLiterals.scala
@@ -0,0 +1,11 @@
+package com.my.scala.chapter5
+
+object BooleanLiterals {
+
+  def main(args: Array[String]) {
+    val bool = true
+    println(bool)
+    val fool = false
+    println(fool)
+  }
+}
