445f07f6ac50372b1e5af1e874d4edfcfc16f844
Dmitry Palaznik <dpalaznik@gmail.com>
2015-03-26 11:29:15 +0300

    Finally block examples in scala

--- src/main/scala/com/my/scala/chapter7/ExceptionHandling.scala
+++ src/main/scala/com/my/scala/chapter7/ExceptionHandling.scala
@@ -6,11 +6,17 @@ object ExceptionHandling {
 
   def main(args: Array[String]) {
     try {
-      val f = new FileReader("input.txt")
+      val file = new FileReader("input.txt")
       // use and close file
     } catch {
       case ex: FileNotFoundException => // Handle missing file
       case ex: IOException => // Handle other I/O error
+    }
+    val file2 = new FileReader("input2.txt")
+    try {
+      // use the file
+    } finally {
+      file2.close()
     }
   }
 
