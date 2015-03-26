24ddb368239d1e2751e58bef85ab9a7eadc5f13a
Dmitry Palaznik <dpalaznik@gmail.com>
2015-03-26 10:20:13 +0300

    Catching exceptions in scala

--- src/main/scala/com/my/scala/chapter7/ExceptionHandling.scala
+++ src/main/scala/com/my/scala/chapter7/ExceptionHandling.scala
@@ -1,6 +1,18 @@
 package com.my.scala.chapter7
 
+import java.io.{IOException, FileNotFoundException, FileReader}
+
 object ExceptionHandling {
+
+  def main(args: Array[String]) {
+    try {
+      val f = new FileReader("input.txt")
+      // use and close file
+    } catch {
+      case ex: FileNotFoundException => // Handle missing file
+      case ex: IOException => // Handle other I/O error
+    }
+  }
 
   def half(n: Int) =
     if (n % 2 == 0)
