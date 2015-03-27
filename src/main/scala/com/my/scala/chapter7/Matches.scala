d004ca00061d10d072a22aab6d0d6b2361273ba1
Dmitry Palaznik <dpalaznik@gmail.com>
2015-03-27 11:57:23 +0300

    Match structures (aka switch-case in java)

---
+++ src/main/scala/com/my/scala/chapter7/Matches.scala
@@ -0,0 +1,26 @@
+package com.my.scala.chapter7
+
+
+object Matches {
+
+  def main(args: Array[String]) {
+
+    val firstArg = if (args.length > 0) args(0) else ""
+    firstArg match {
+      case "salt" => println("pepper")
+      case "chips" => println("salsa")
+      case "eggs" => println("bacon")
+      case _ => println("huh?")
+    }
+
+    val friend =
+      firstArg match {
+        case "salt" => "pepper"
+        case "chips" => "salsa"
+        case "eggs" => "bacon"
+        case _ => "huh?"
+      }
+
+    println(friend)
+  }
+}
