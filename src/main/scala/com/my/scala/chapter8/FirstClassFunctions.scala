bf53be728e423e0785df3ac43abf2ab3e20cb731
Dmitry Palaznik <dpalaznik@gmail.com>
2015-04-01 11:17:57 +0300

    First class functions usage with filter method

--- src/main/scala/com/my/scala/chapter8/FirstClassFunctions.scala
+++ src/main/scala/com/my/scala/chapter8/FirstClassFunctions.scala
@@ -20,5 +20,7 @@ object FirstClassFunctions {
 
     val someNumbers = List(-11, -10, -5, 0, 5, 10)
     someNumbers.foreach((x: Int) => println(x))
+
+    println(someNumbers.filter((x: Int) => x > 0))
   }
 }
