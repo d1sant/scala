3b80753afee5ddf6b1934c826a2d0f02520bb0bc
Dmitry Palaznik <dpalaznik@gmail.com>
2015-04-01 11:03:07 +0300

    First class functions usage

---
+++ src/main/scala/com/my/scala/chapter8/FirstClassFunctions.scala
@@ -0,0 +1,21 @@
+package com.my.scala.chapter8
+
+object FirstClassFunctions {
+
+  def main(args: Array[String]) {
+
+    var increase = (x: Int) => x + 1
+    println(increase(10))
+
+    increase = (x: Int) => x + 9999
+    println(increase(10))
+
+    increase = (x: Int) => {
+      println("We")
+      println("are")
+      println("here!")
+      x + 1
+    }
+    println(increase(10))
+  }
+}
