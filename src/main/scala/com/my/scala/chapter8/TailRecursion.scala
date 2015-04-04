b02662101d216a0833c4ec8e4bfa74a3eba56abc
Dmitry Palaznik <dpalaznik@gmail.com>
2015-04-04 12:06:08 +0300

    Restrictions of tail recursion

--- src/main/scala/com/my/scala/chapter8/TailRecursion.scala
+++ src/main/scala/com/my/scala/chapter8/TailRecursion.scala
@@ -40,4 +40,18 @@ object TailRecursion {
   def bang(x: Int): Int =
     if (x == 0) throw new Exception("bang!")
     else bang(x - 1)
+
+  // LIMITS of tail recursion
+  // a) indirect recursion:
+  def isEven(x: Int): Boolean =
+    if (x == 0) true else isOdd(x - 1)
+
+  def isOdd(x: Int): Boolean =
+    if (x == 0) false else isEven(x - 1)
+  
+  // b) function value:
+  val funValue = nestedFun _
+  def nestedFun(x: Int): Unit = {
+    if (x != 0) { println(x); funValue(x - 1) }
+  }
 }
