c7d944a85547fdd09d89bd00e0c68a37ec8549a1
Dmitry Palaznik <dpalaznik@gmail.com>
2015-04-06 11:56:44 +0300

    WontCompile example with the same name of field and method

--- src/main/scala/com/my/scala/chapter10/ArrayElement.scala
+++ src/main/scala/com/my/scala/chapter10/ArrayElement.scala
@@ -1,8 +1,5 @@
 package com.my.scala.chapter10
 
-/**
- * In Scala it's forbidden to define methods and fields with the same names (in Java it's possible)
- */
 class ArrayElement(conts: Array[String]) extends Element {
   val contents: Array[String] = conts
 }
---
+++ src/main/scala/com/my/scala/chapter10/WontCompile.scala
@@ -0,0 +1,9 @@
+package com.my.scala.chapter10
+
+/**
+ * In Scala it's forbidden to define methods and fields with the same names (in Java it's possible)
+ */
+class WontCompile {
+  // private var f = 0
+  // def f = 1
+}
