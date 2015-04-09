d984b4175c7e3bc946cc518556a42668264c3bb0
Dmitry Palaznik <dpalaznik@gmail.com>
2015-04-09 10:26:47 +0300

    Polymorphism and dynamic binding

--- src/main/scala/com/my/scala/chapter10/Elements.scala
+++ src/main/scala/com/my/scala/chapter10/Elements.scala
@@ -8,8 +8,16 @@ object Elements {
     val ae = new ArrayElement(Array("hello", "world"))
     println(ae.width)
 
-    // subtyping
-    val e: Element = new ArrayElement(Array("hello"))
-    println(e.width)
+    // sub typing 1
+    val e1: Element = new ArrayElement(Array("hello"))
+    println(e1.width)
+
+    // sub typing 2
+    val e2: Element = new LineElement("hi")
+    println(e2.width)
+
+    // sub typing 3
+    val e3: Element = new UniformElement('x', 2, 3)
+    println(e3.width)
   }
 }
