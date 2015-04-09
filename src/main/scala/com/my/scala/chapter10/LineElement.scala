c65b88b216b0368d40703146641eb035df13429a
Dmitry Palaznik <dpalaznik@gmail.com>
2015-04-09 10:35:36 +0300

    Final modifiers

--- src/main/scala/com/my/scala/chapter10/LineElement.scala
+++ src/main/scala/com/my/scala/chapter10/LineElement.scala
@@ -1,9 +1,9 @@
 package com.my.scala.chapter10
 
-class LineElement(s: String) extends ArrayElement(Array(s)) {
+final class LineElement(s: String) extends ArrayElement(Array(s)) {
   override def width = s.length
   override def height = 1
-  override def demo(): Unit = {
+  final override def demo(): Unit = {
     println("LineElement's implementation invoked")
   }
 }
