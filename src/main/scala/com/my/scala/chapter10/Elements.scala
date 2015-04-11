6014438730b1b454dc2d494fc195398fe2204570
Dmitry Palaznik <dpalaznik@gmail.com>
2015-04-11 13:11:32 +0300

    Some examples of unsufficient work of current implementation

--- src/main/scala/com/my/scala/chapter10/Elements.scala
+++ src/main/scala/com/my/scala/chapter10/Elements.scala
@@ -6,6 +6,8 @@ object Elements {
 
   def main(args: Array[String]) {
 
+    // Won't properly work
     println(elem("Hello") above elem("world!"))
+    println(elem(Array("one", "two")) beside elem(Array("one")))
   }
 }
