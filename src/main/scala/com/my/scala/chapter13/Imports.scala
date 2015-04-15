230aab9470a5dc217b6e684266f51a89ea3054a7
Dmitry Palaznik <dpalaznik@gmail.com>
2015-04-15 11:20:27 +0300

    Some more examples of imports

--- src/main/scala/com/my/scala/chapter13/Imports.scala
+++ src/main/scala/com/my/scala/chapter13/Imports.scala
@@ -28,27 +28,44 @@ object Imports {
   }
 
   def showFruit(fruit: Fruit): Unit = {
-
     // importing of the members of a regular (not singleton) object
     import fruit._
-
     println(name + "s are " + color)
   }
 
   def hideSomeMembers() {
-    
     import bobsdelights.Fruits.{Apple, Orange}
-
     val apple = Apple
     val orange = Orange
     // Pear // is not visible
   }
 
   def renameMembers() {
-
     import Fruits.{Apple => McIntosh, Orange}
-
     val mcIntosh = McIntosh
     val orange = Orange
+  }
+
+  def renamePackage() {
+    import java.{sql => S}
+    val date:S.Date = null
+  }
+
+  def showFruits() {
+    import Fruits.{_}
+    val apple = Apple
+  }
+
+  def showFruitsWithRename() {
+    import Fruits.{Apple => McIntosh,_}
+    val mcIntosh = McIntosh
+    val orange = Orange
+  }
+
+  def showFruitsExceptOff(): Unit = {
+    import Fruits.{Pear => _, _}
+    val apple = Apple
+    val orange = Orange
+    // Pear is not visible
   }
 }
