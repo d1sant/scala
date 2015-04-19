c1f49787b1b4f842d1598e98b943200e66ad0568
Dmitry Palaznik <dpalaznik@gmail.com>
2015-04-19 13:12:21 +0300

    Informative failure reports

--- src/test/scala/com/my/scala/chapter14/ElementSuite.scala
+++ src/test/scala/com/my/scala/chapter14/ElementSuite.scala
@@ -4,8 +4,36 @@ import org.scalatest.Suite
 import Element.elem
 
 class ElementSuite extends Suite {
+
   def testUniformElement() = {
     val ele = elem('x', 2, 3)
     assert(ele.width == 2)
   }
+
+  // Informative failure report
+  def testUniformElement2() = {
+    val ele = elem('x', 2, 3)
+    assert(ele.width === 2)
+  }
+
+  // Informative failure report with expect() (deprecated)
+  def testUniformElement3() = {
+    val ele = elem('x', 2, 3)
+    expect(2) {
+      ele.width
+    }
+  }
+
+  // Informative failure report with assertResult()
+  def testUniformElement4() = {
+    val ele = elem('x', 2, 3)
+    assertResult(2) {
+      ele.width
+    }
+  }
+
+  // Will throw TestFailedException cause elem method doesn't throw exceptions
+  // intercept[IllegalArgumentException] {
+  //   elem('x', -2, 3)
+  // }
 }
