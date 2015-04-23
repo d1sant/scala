6c146ecb2b3e42be769adcec6b1ffa6428740580
Dmitry Palaznik <dpalaznik@gmail.com>
2015-04-23 16:05:17 +0300

    Constant pattern in case classes

--- src/main/scala/com/my/scala/chapter15/CaseClasses.scala
+++ src/main/scala/com/my/scala/chapter15/CaseClasses.scala
@@ -31,5 +31,27 @@ object CaseClasses {
 
     import Printer.print
     println(print(binOp))
+
+    // patterns
+    println(describe(5))
+    println(describe(true))
+    println(describe("hello"))
+    println(describe(Nil))
+    println(describe(List(1,2,3)))
+  }
+
+  // wildcard patterns
+  def print2(expr: Expr) = expr match {
+    case BinOp(_, _, _) => println(expr + " is a binary operator")
+    case _ => println("It's something else")
+  }
+
+  // constant patterns
+  def describe(x: Any) = x match {
+    case 5 => "five"
+    case true => "truth"
+    case "hello" => "hi!"
+    case Nil => "the empty list"
+    case _ => "something else"
   }
 }
--- src/main/scala/com/my/scala/chapter15/Printer.scala
+++ src/main/scala/com/my/scala/chapter15/Printer.scala
@@ -7,10 +7,4 @@ object Printer {
       println(expr + " is a binary operator")
     case _ =>
   }
-
-  // using wildcard pattern
-  def print2(expr: Expr) = expr match {
-    case BinOp(_, _, _) => println(expr + " is a binary operator")
-    case _ => println("It's something else")
-  }
 }
