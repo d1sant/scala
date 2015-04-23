defdc85d796f81b9748f4ed001d96ce5ea2cd1f2
Dmitry Palaznik <dpalaznik@gmail.com>
2015-04-23 15:53:38 +0300

    Wildcard pattern in case classes

--- src/main/scala/com/my/scala/chapter15/Printer.scala
+++ src/main/scala/com/my/scala/chapter15/Printer.scala
@@ -1,9 +1,16 @@
 package com.my.scala.chapter15
 
 object Printer {
+
   def print(expr: Expr) = expr match {
     case BinOp(op, left, right) =>
       println(expr + " is a binary operator")
     case _ =>
+  }
+
+  // using wildcard pattern
+  def print2(expr: Expr) = expr match {
+    case BinOp(_, _, _) => println(expr + " is a binary operator")
+    case _ => println("It's something else")
   }
 }
