a4e597e5be347a9a29cb062bc33591a2af7b54da
Dmitry Palaznik <dpalaznik@gmail.com>
2015-05-04 13:24:23 +0300

    Merge sort implementation and usage examples

--- src/main/scala/com/my/scala/chapter16/Lists.scala
+++ src/main/scala/com/my/scala/chapter16/Lists.scala
@@ -137,8 +137,20 @@ object Lists {
     println(arr2.mkString(" "))
 
     val it = abcde.iterator
-    println(it.next)
-    println(it.next)
+    println(it.next())
+    println(it.next())
+
+    // merge sort usage
+    println(msort((x: Int, y: Int) => x < y)(List(5, 7, 1, 3)))
+
+    val intSort = msort((x: Int, y: Int) => x < y) _
+    println(intSort)
+    val reverseIntSort = msort((x: Int, y: Int) => x > y) _
+    println(reverseIntSort)
+
+    val mixedInts = List(4, 1, 9, 0, 5, 8, 3, 6, 2, 7)
+    println(intSort(mixedInts))
+    println(reverseIntSort(mixedInts))
   }
 
   /**
@@ -175,5 +187,23 @@ object Lists {
   def rev[T](xs: List[T]): List[T] = xs match {
     case List() => xs
     case x :: xs1 => rev(xs1) ::: List(x)
+  }
+
+  def msort[T](less: (T, T) => Boolean)
+    (xs: List[T]): List[T] = {
+    def merge(xs: List[T], ys: List[T]): List[T] =
+      (xs, ys) match {
+        case (Nil, _) => ys
+        case (_, Nil) => xs
+        case (x :: xs1, y :: ys1) =>
+          if (less(x, y)) x :: merge(xs1, ys)
+          else y :: merge(xs, ys1)
+      }
+    val n = xs.length / 2
+    if (n == 0) xs
+    else {
+      val (ys, zs) = xs splitAt n
+      merge(msort(less)(ys), msort(less)(zs))
+    }
   }
 }
