7784a3201c2f83c273c939a8addaf8dd4ae68ac2
Dmitry Palaznik <dpalaznik@gmail.com>
2015-05-12 10:48:44 +0300

    Sequences: lists as example

---
+++ src/main/scala/com/my/scala/chapter17/Collections.scala
@@ -0,0 +1,14 @@
+package com.my.scala.chapter17
+
+object Collections {
+
+  def main(args: Array[String]) {
+
+    // Sequences
+    // Lists:
+    val colors = List("red", "blue", "green")
+    println(colors)
+    println(colors.head)
+    println(colors.tail)
+  }
+}
