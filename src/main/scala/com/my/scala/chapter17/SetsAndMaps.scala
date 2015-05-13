aaa6afb51a53acf7dc3e6f93cbf76e1815dd4d37
Dmitry Palaznik <dpalaznik@gmail.com>
2015-05-13 10:00:35 +0300

    Sets and Maps: sorted sets and maps

--- src/main/scala/com/my/scala/chapter17/SetsAndMaps.scala
+++ src/main/scala/com/my/scala/chapter17/SetsAndMaps.scala
@@ -106,6 +106,21 @@ object SetsAndMaps {
     // 3 ->         scala.collection.immutable.Map3
     // 4 ->         scala.collection.immutable.Map4
     // 5 or more -> scala.collection.immutable.HashMap
+
+    println("-- Sorted sets and maps")
+    import scala.collection.immutable.TreeSet
+    val ts = TreeSet(9, 3, 1, 8, 0, 2, 7, 4, 6, 5)
+    println(ts)
+
+    val cs = TreeSet('f', 'u', 'n')
+    println(cs)
+
+    import scala.collection.immutable.TreeMap
+    var tm = TreeMap(3 -> 'x', 1 -> 'x', 4 -> 'x')
+    println(tm)
+
+    tm += (2 -> 'x')
+    println(tm)
   }
 
   def countWords(text: String) = {
