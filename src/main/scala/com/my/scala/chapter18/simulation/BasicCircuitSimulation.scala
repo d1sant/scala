55e7bfb212ba820407dadadc64f24a8735a2e83d
Dmitry Palaznik <dpalaznik@gmail.com>
2015-05-19 19:32:33 +0300

    Stateful Objects: MySimulation as usage example

--- src/main/scala/com/my/scala/chapter18/simulation/BasicCircuitSimulation.scala
+++ src/main/scala/com/my/scala/chapter18/simulation/BasicCircuitSimulation.scala
@@ -19,42 +19,42 @@ abstract class BasicCircuitSimulation extends Simulation {
       actions = a :: actions
       a()
     }
-    def inverter(input: Wire, output: Wire) = {
-      def inverterAction(): Unit = {
-        val inputSig = input.getSignal
-        afterDelay(InverterDelay) {
-          output setSignal !inputSig
-        }
+  }
+  def inverter(input: Wire, output: Wire) = {
+    def inverterAction(): Unit = {
+      val inputSig = input.getSignal
+      afterDelay(InverterDelay) {
+        output setSignal !inputSig
       }
-      input addAction inverterAction
     }
-    def andGate(a1: Wire, a2: Wire, output: Wire) = {
-      def andAction() = {
-        val a1Sig = a1.getSignal
-        val a2Sig = a2.getSignal
-        afterDelay(AndGateDelay) {
-          output setSignal (a1Sig & a2Sig)
-        }
+    input addAction inverterAction
+  }
+  def andGate(a1: Wire, a2: Wire, output: Wire) = {
+    def andAction() = {
+      val a1Sig = a1.getSignal
+      val a2Sig = a2.getSignal
+      afterDelay(AndGateDelay) {
+        output setSignal (a1Sig & a2Sig)
       }
-      a1 addAction andAction
-      a2 addAction andAction
     }
-    def orGate(o1: Wire, o2: Wire, output: Wire) = {
-      def orAction() {
-        val o1Sig = o1.getSignal
-        val o2Sig = o2.getSignal
-        afterDelay(OrGateDelay) {
-          output setSignal (o1Sig | o2Sig)
-        }
+    a1 addAction andAction
+    a2 addAction andAction
+  }
+  def orGate(o1: Wire, o2: Wire, output: Wire) = {
+    def orAction() {
+      val o1Sig = o1.getSignal
+      val o2Sig = o2.getSignal
+      afterDelay(OrGateDelay) {
+        output setSignal (o1Sig | o2Sig)
       }
-      o1 addAction orAction
-      o2 addAction orAction
     }
-    def probe(name: String, wire: Wire) {
-      def probeAction() = {
-        println(name + " " + currentTime + " new-value = " + wire.getSignal)
-      }
-      wire addAction probeAction
+    o1 addAction orAction
+    o2 addAction orAction
+  }
+  def probe(name: String, wire: Wire) {
+    def probeAction() = {
+      println(name + " " + currentTime + " new-value = " + wire.getSignal)
     }
+    wire addAction probeAction
   }
 }
