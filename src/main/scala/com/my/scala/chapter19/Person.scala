a795ca602f8e4462ffe544107097e69a74fbc2bc
Dmitry Palaznik <dpalaznik@gmail.com>
2015-07-28 15:27:03 +0300

    Type parameterization: upper bounds

---
+++ src/main/scala/com/my/scala/chapter19/Person.scala
@@ -0,0 +1,54 @@
+package com.my.scala.chapter19
+
+class Person(val firstName: String, val lastName: String) extends Ordered[Person] {
+  def compare(that: Person) = {
+    val lastNameComparison = lastName.compareToIgnoreCase(that.lastName)
+    if (lastNameComparison != 0)
+      lastNameComparison
+    else
+      firstName.compareToIgnoreCase(that.firstName)
+  }
+  override def toString = firstName + " " + lastName
+}
+
+object Person {
+
+  def main(args: Array[String]) {
+
+    val robert = new Person("Robert", "Jones")
+    val sally = new Person("Sally", "Smith")
+    println(robert)
+    println(sally)
+    println(robert < sally)
+
+    val people = List(
+      new Person("Larry", "Wall"),
+      new Person("Anders", "Hejlsberg"),
+      new Person("Guido", "van Rossum"),
+      new Person("Alan", "Kay"),
+      new Person("Yukihiro", "Matsumoto")
+    )
+    println(people)
+
+    val sortedPeople = orderedMergeSort(people)
+    println(sortedPeople)
+
+    // val wontCompile = orderedMergeSort(List(3, 2, 1)) // won't compile cause of absence of Ordered mix
+  }
+  def orderedMergeSort[T <: Ordered[T]](xs: List[T]): List[T] = {
+    def merge(xs: List[T], ys: List[T]): List[T] =
+      (xs, ys) match {
+        case (Nil, _) => ys
+        case (_, Nil) => xs
+        case (x :: xs1, y :: ys1) =>
+          if (x < y) x :: merge(xs1, ys)
+          else y :: merge(xs, ys1)
+      }
+    val n = xs.length / 2
+    if (n == 0) xs
+    else {
+      val (ys, zs) = xs splitAt n
+      merge(orderedMergeSort(ys), orderedMergeSort(zs))
+    }
+  }
+}
