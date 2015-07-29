316d063b6f2e67a7a5404493d148fd79f7733ee1
Dmitry Palaznik <dpalaznik@gmail.com>
2015-07-29 10:43:23 +0300

    Abstract members: abstract vals

---
+++ src/main/scala/com/my/scala/chapter20/Fruit.scala
@@ -0,0 +1,16 @@
+package com.my.scala.chapter20
+
+abstract class Fruit {
+  val v: String // 'v' for value
+  def m: String // 'm' for method
+}
+
+abstract class Apple extends Fruit {
+  val v: String
+  val m: String // OK to override a 'def' with a 'val'
+}
+
+abstract class BadApple extends Fruit {
+  def v: String // ERROR: cannot override a 'val' with a 'def'
+  def m: String
+}
