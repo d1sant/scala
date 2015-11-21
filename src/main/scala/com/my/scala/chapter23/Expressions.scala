35f59092c91e0a7524c57e8415c974f70a7a2faa
Dmitry Palaznik <dpalaznik@gmail.com>
2015-11-21 16:00:10 +0300

    For expressions revisited: querying with for expressions

--- src/main/scala/com/my/scala/chapter23/Expressions.scala
+++ src/main/scala/com/my/scala/chapter23/Expressions.scala
@@ -10,7 +10,7 @@ object Expressions {
     val persons = List(lara, bob, julie)
 
     println(persons filter (p => !p.isMale) flatMap (p => (p.children map (c => (p.name, c.name)))))
-    println(persons withFilter(p => !p.isMale) flatMap (p => (p.children map (c => (p.name, c.name)))))
+    println(persons withFilter (p => !p.isMale) flatMap (p => (p.children map (c => (p.name, c.name)))))
 
     // easies way to comprehend:
     println(for (p <- persons; if !p.isMale; c <- p.children) yield (p.name, c.name))
@@ -39,9 +39,60 @@ object Expressions {
 
     def inCheck(q1: (Int, Int), q2: (Int, Int)) =
       q1._1 == q2._1 || // the same row
-      q1._2 == q2._2 || // the same column
-      (q1._1 - q2._1).abs == (q1._2 - q2._2).abs // on diagonal
+        q1._2 == q2._2 || // the same column
+        (q1._1 - q2._1).abs == (q1._2 - q2._2).abs // on diagonal
+
+    // Querying with for expressions
+
+    val books: List[Book] =
+      List(
+        Book(
+          "Structure and Interpretation of Computer Programs",
+          "Abelson, Harold", "Sussman, Gerald J."
+        ),
+        Book(
+          "Principles of Computer Design",
+          "Aho, Alfred", "Ullman, Jeffrey"
+        ),
+        Book(
+          "Programming of Modular-2",
+          "Wirth, Niklaus"
+        ),
+        Book(
+          "Elements of ML Programming",
+          "Ullman, Jeffrey"
+        ),
+        Book(
+          "The Java Language Specification",
+          "Gosling, James", "Joy, Bill", "Steel, Guy", "Bracha, Gilad"
+        )
+      )
+
+    println(for (b <- books; a <- b.authors if a startsWith ("Gosling")) yield b.title)
+    println(for (b <- books if (b.title indexOf "Program") >= 0) yield b.title)
+    println(for (b1 <- books; b2 <- books if b1 != b2; a1 <- b1.authors; a2 <- b2.authors if a1 == a2) yield a1)
+    println(removeDublicates(for (b1 <- books; b2 <- books if b1 != b2; a1 <- b1.authors; a2 <- b2.authors if a1 == a2) yield a1))
+    println(removeDublicates2(for (b1 <- books; b2 <- books if b1 != b2; a1 <- b1.authors; a2 <- b2.authors if a1 == a2) yield a1))
+
+    def removeDublicates[A](xs: List[A]): List[A] = {
+      if (xs.isEmpty) xs
+      else
+        xs.head :: removeDublicates(
+          xs.tail filter (x => x != xs.head)
+        )
+    }
+
+    def removeDublicates2[A](xs: List[A]): List[A] = {
+      if (xs.isEmpty) xs
+      else
+        xs.head :: removeDublicates2(
+          for (x <- xs.tail if x != xs.head) yield x
+        )
+    }
   }
 
   case class Person(name: String, isMale: Boolean, children: Person*)
+
+  case class Book(title: String, authors: String*)
+
 }
