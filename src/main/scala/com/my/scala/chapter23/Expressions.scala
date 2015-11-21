a18ba59ab09a4ac2a8a068e4c38caa65cf71fee8
Dmitry Palaznik <dpalaznik@gmail.com>
2015-11-21 15:37:20 +0300

    For expressions revisited: the n-queens problem

--- src/main/scala/com/my/scala/chapter23/Expressions.scala
+++ src/main/scala/com/my/scala/chapter23/Expressions.scala
@@ -20,6 +20,27 @@ object Expressions {
 
     // several generators: later generators vary more rapidly than earlier ones
     println(for (x <- List(1, 2); y <- List("one", "two")) yield (x, y))
+
+    def queens(n: Int): List[List[(Int, Int)]] = {
+      def placeQueens(k: Int): List[List[(Int, Int)]] =
+        if (k == 0)
+          List(List())
+        else
+          for {
+            queens <- placeQueens(k - 1)
+            column <- 1 to n
+            queen = (k, column)
+            if isSafe(queen, queens)
+          } yield queen :: queens
+      placeQueens(n)
+    }
+
+    def isSafe(queen: (Int, Int), queens: List[(Int, Int)]) = queens forall (q => !inCheck(queen, q))
+
+    def inCheck(q1: (Int, Int), q2: (Int, Int)) =
+      q1._1 == q2._1 || // the same row
+      q1._2 == q2._2 || // the same column
+      (q1._1 - q2._1).abs == (q1._2 - q2._2).abs // on diagonal
   }
 
   case class Person(name: String, isMale: Boolean, children: Person*)
