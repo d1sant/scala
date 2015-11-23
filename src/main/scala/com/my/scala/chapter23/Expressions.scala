3f6da655bc0e044b1d44112f187466328dae3466
Dmitry Palaznik <dpalaznik@gmail.com>
2015-11-23 10:09:10 +0300

    For expressions revisited: translation of for expressions

--- src/main/scala/com/my/scala/chapter23/Expressions.scala
+++ src/main/scala/com/my/scala/chapter23/Expressions.scala
@@ -89,6 +89,68 @@ object Expressions {
           for (x <- xs.tail if x != xs.head) yield x
         )
     }
+
+    // Translation of for expressions
+
+    // One generator:
+    // for (x <- expr1) yield expr2
+    // expr1.map(x => expr2)
+
+    // A generator and a filter:
+    // for (x <- expr1 if expr2) yield expr3
+    // for (x <- expr1 withFilter (x => expr2)) yield expr3
+    // expr1 withFilter (x => expr2) map (x => expr3)
+    // or:
+    // for (x <- expr1 if expr2; seq) yield expr3
+    // for (x <- expr1 withFilter expr2; seq) yield expr3
+
+    // Two generators:
+    // for (x <- expr1; y <- expr2; seq) yield expr3
+    // expr1.flatMap(x => for (y <- expr2; seq) yield expr3)
+
+    for (b1 <- books; b2 <- books if b1 != b2;
+         a1 <- b1.authors; a2 <- b2.authors if a1 == a2)
+      yield a1
+
+    books flatMap(b1 =>
+      books withFilter (b2 => b1 != b2) flatMap (b2 =>
+        b1.authors flatMap (a1 =>
+          b2.authors withFilter (a2 => a1 == a2) map (a2 =>
+            a1)
+          )
+        )
+      )
+
+    // Translating patterns in generators
+    // for ((x1, ..., xn) <- expr1) yield expr2
+    // expr1.map {case (x1, ..., xn) => expr2}
+
+    // for (pat <- expr1) yield expr2
+    // expr1 withFilter {
+    // case pat => true
+    // case _=> false
+    // } map {
+    // case pat => expr2
+    // }
+
+    // Translating definitions:
+    // for (x <- expr1; y = expr2; seq) yield expr3
+    // for ((x, y) <- for (x <- expr1) yield (x, expr2); seq)
+    // yield expr3
+
+    // for (x <- 1 to 1000; y = expensiveComputationNotInvolvingX)
+    // yield x * y
+    // it's usually better to write:
+    // val y = expensiveComputationNotInvolvingX
+    // for (x <- 1 to 1000) yield x * y
+
+    // Translating for loops:
+    // for (x <- expr1) body
+    // expr1 foreach (x => body)
+
+    // for (x <- expr1; if expr2; y <- expr3) body
+    // expr1 withFilter (x => expr2) foreach (x =>
+    //  expr3 foreach (y => body))
   }
 
   case class Person(name: String, isMale: Boolean, children: Person*)
