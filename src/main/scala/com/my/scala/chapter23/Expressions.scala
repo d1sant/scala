c19507064399e9e45c83482ec9d12d5506c36737
Dmitry Palaznik <dpalaznik@gmail.com>
2015-11-23 10:15:16 +0300

    For expressions revisited: going the other way

--- src/main/scala/com/my/scala/chapter23/Expressions.scala
+++ src/main/scala/com/my/scala/chapter23/Expressions.scala
@@ -158,3 +158,12 @@ object Expressions {
   case class Book(title: String, authors: String*)
 
 }
+
+object Demo {
+  def map[A, B](xs: List[A], f: A => B): List[B] =
+    for (x <- xs) yield f(x)
+  def flatMap[A, B](xs: List[A], f: A => List[B]): List[B] =
+    for (x <- xs; y <- f(x)) yield y
+  def filter[A](xs: List[A], p: A => Boolean): List[A] =
+    for (x <- xs if p(x)) yield x
+}
