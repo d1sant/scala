a54bc2dd6b3bd628eef6001ae72e98ddf33f7645
Dmitry Palaznik <dpalaznik@gmail.com>
2015-12-01 10:53:37 +0300

    The Architecture of Scala Collections: builders and factoring out common operations

--- src/main/scala/com/my/scala/chapter25/CollectionsAchitecture.scala
+++ src/main/scala/com/my/scala/chapter25/CollectionsAchitecture.scala
@@ -1,5 +1,26 @@
 package com.my.scala.chapter25
 
+import scala.collection.mutable.ArrayBuffer
+
 object CollectionsAchitecture {
 
+  def main(args: Array[String]) {
+
+    val buf = new ArrayBuffer[Int]
+    val bldr = buf mapResult(_.toArray)
+
+    // BitSet problem
+    import collection.immutable.BitSet
+    val bits = BitSet(1, 2, 3)
+    println(bits map (_ * 2)) // produces bit set
+    println(bits map (_.toFloat)) // produces set
+
+    println(Map("a" -> 1, "b" -> 2) map {case (x, y) => (y, x)})
+    println(Map("a" -> 1, "b" -> 2) map {case (x, y) => y})
+
+    val xs: Iterable[Int] = List(1, 2, 3)
+    println(xs)
+    val ys = xs map (x => x * x)
+    println(ys)
+  }
 }
