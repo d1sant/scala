87cffbd8bdcc63ba9046ff437e872c9e1edfc9c4
Dmitry Palaznik <dpalaznik@gmail.com>
2015-12-02 11:14:49 +0300

    Extractors: regular expressions

--- src/main/scala/com/my/scala/chapter26/Extractors.scala
+++ src/main/scala/com/my/scala/chapter26/Extractors.scala
@@ -50,6 +50,33 @@ object Extractors {
     println(name)
     println(topdom)
     println(subdoms)
+
+    import scala.util.matching.Regex
+    val Decimal = new Regex("(-)?(\\d+)(\\.\\d*)?")
+    println(Decimal)
+    val Decimal1 = new Regex("""(-)?(\d+)(\.\d*)?""")
+    println(Decimal1)
+    val Decimal2 = """(-)?(\d+)(\.\d*)?""".r
+    println(Decimal2)
+
+    val input = "for -1.0 to 99 by 3"
+    for (s <- Decimal findAllIn( input))
+      println(s)
+    println(Decimal findFirstIn input)
+    println(Decimal findPrefixOf input)
+
+    val Decimal(sign, integerpart, decimalpart) = "-1.23"
+    println(sign)
+    println(integerpart)
+    println(decimalpart)
+
+    val Decimal(sign1, integerpart1, decimalpart1) = "1.0"
+    println(sign1)
+    println(integerpart1)
+    println(decimalpart1)
+
+    for (Decimal(s, i, d) <- Decimal findAllIn input)
+      println("sign: " + s + ", integer: " + i + ", decimal: " + d)
   }
 }
 
