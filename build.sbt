41a85a554c7fd2b4993df05aabeced39cc70e9fe
Dmitry Palaznik <dpalaznik@gmail.com>
2015-12-02 15:31:42 +0300

    Working with XML: XML literals and serialization

--- build.sbt
+++ build.sbt
@@ -13,3 +13,5 @@ libraryDependencies += "org.testng" % "testng" % "6.2" % "test"
 libraryDependencies += "org.scala-tools.testing" % "specs_2.10" % "1.6.9" % "test"
 
 libraryDependencies += "org.scalacheck" %% "scalacheck" % "1.12.2" % "test"
+
+libraryDependencies += "org.scala-lang.modules" %% "scala-xml" % "1.0.2"
---
+++ src/main/scala/com/my/scala/chapter28/XMLs.scala
@@ -0,0 +1,56 @@
+package com.my.scala.chapter28
+
+object XMLs {
+
+  def main(args: Array[String]) {
+
+    println(
+      <a>
+        This is some XML.
+        Here is a tag: <atag/>
+      </a>
+    )
+
+    println(<a> {"Hello" + ", world"} </a>)
+    val yearMade = 1955
+    println(
+      <a> { if (yearMade < 2000) <old>{yearMade}</old>
+            else xml.NodeSeq.Empty }
+      </a>
+    )
+    println(<a> {3 + 4} </a>)
+    println(<a> {"</a>potential security hole<a>"}</a>)
+    println("<a>" + "</a>potential security hole<a>" + "</a>")
+
+    val therm = new CCTherm {
+      override val description: String = "hot dog #5"
+      override val yearMade: Int = 1952
+      override val dateObtained: String = "March 14, 2006"
+      override val bookPrice: Int = 2199
+      override val purchasePrice: Int = 500
+      override val condition: Int = 9
+    }
+    println(therm.toXML)
+    println(<a> {{{{brace yourself!}}}} </a>)
+  }
+
+  abstract class CCTherm {
+    val description: String
+    val yearMade: Int
+    val dateObtained: String
+    val bookPrice: Int       // in US cents
+    val purchasePrice: Int   // in US cents
+    val condition: Int       // 1 to 10
+    override def toString = description
+
+    def toXML =
+     <cctherm>
+       <description>{description}</description>
+       <yearMade>{yearMade}</yearMade>
+       <dateObtained>{dateObtained}</dateObtained>
+       <bookPrice>{bookPrice}</bookPrice>
+       <purchasePrice>{purchasePrice}</purchasePrice>
+       <condition>{condition}</condition>
+     </cctherm>
+  }
+}
