28c023c5ca19be4c6171369f84ebb681e23fb19e
Dmitry Palaznik <dpalaznik@gmail.com>
2015-12-02 17:55:42 +0300

    Working with XML: pattern matching on XML

--- src/main/scala/com/my/scala/chapter28/XMLs.scala
+++ src/main/scala/com/my/scala/chapter28/XMLs.scala
@@ -58,6 +58,63 @@ object XMLs {
     val loadNode = xml.XML.loadFile("therm1.xml")
     println(loadNode)
     println(therm.fromXML(loadNode))
+
+    // Pattern matching
+    def proc(node: scala.xml.Node): String =
+      node match {
+        case <a>{contents}</a> => "It's an a: " + contents
+        case <b>{contents}</b> => "It's a b: " + contents
+        case _ => "It's something else."
+      }
+
+    println(proc(<a>apple</a>))
+    println(proc(<b>banana</b>))
+    println(proc(<c>cherry</c>))
+
+    println(proc(<a>a <em>red</em> apple</a>))
+    println(proc(<a/>))
+
+    def proc2(node: scala.xml.Node): String =
+      node match {
+        case <a>{contents @ _*}</a> => "It's an a: " + contents
+        case <b>{contents @ _*}</b> => "It's a b: " + contents
+        case _ => "It's something else."
+      }
+
+    println(proc2(<a>a <em>red</em> apple</a>))
+    println(proc2(<a/>))
+
+    val catalog =
+      <catalog>
+        <cctherm>
+          <description>hot dog #5</description>
+          <yearMade>1952</yearMade>
+          <dateObtained>March 14, 2006</dateObtained>
+          <bookPrice>2199</bookPrice>
+          <purchasePrice>500</purchasePrice>
+          <condition>9</condition>
+        </cctherm>
+        <cctherm>
+          <description>Sprite Boy</description>
+          <yearMade>1964</yearMade>
+          <dateObtained>April 28, 2003</dateObtained>
+          <bookPrice>1695</bookPrice>
+          <purchasePrice>595</purchasePrice>
+          <condition>5</condition>
+        </cctherm>
+      </catalog>
+
+    catalog match {
+      case <catalog>{therms @ _*}</catalog> =>
+        for (therm <- therms)
+          println("processing: " + (therm \ "description").text)
+    }
+
+    catalog match {
+      case <catalog>{therms @ _*}</catalog> =>
+        for (therm @ <cctherm>{_*}</cctherm> <- therms)
+          println("processing: " + (therm \ "description").text)
+    }
   }
 
   abstract class CCTherm {
