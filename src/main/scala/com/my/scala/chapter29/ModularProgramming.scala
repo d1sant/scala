a1ca3e41f7ffa3e05e1c757096c5db07b5b49c1f
Dmitry Palaznik <dpalaznik@gmail.com>
2015-12-03 11:05:14 +0300

    Modular Programming Using Objects: abstraction

--- src/main/scala/com/my/scala/chapter29/ModularProgramming.scala
+++ src/main/scala/com/my/scala/chapter29/ModularProgramming.scala
@@ -1,12 +1,17 @@
 package com.my.scala.chapter29
 
-import com.my.scala.chapter29.org.stairwaybook.recipe.{SimpleBrowser, SimpleDatabase}
+import com.my.scala.chapter29.org.stairwaybook.recipe.{SimpleBrowser2, SimpleDatabase2, SimpleBrowser, SimpleDatabase}
 
 object ModularProgramming {
 
   def main(args: Array[String]) {
+
     val apple = SimpleDatabase.foodNamed("Apple").get
     println(apple)
     println(SimpleBrowser.recipesUsing(apple))
+
+    val apple2 = SimpleDatabase2.foodNamed("Apple").get
+    println(apple2)
+    println(SimpleBrowser2.recipesUsing(apple2))
   }
 }
--- src/main/scala/com/my/scala/chapter29/org/stairwaybook/recipe/SimpleDatabase.scala
+++ src/main/scala/com/my/scala/chapter29/org/stairwaybook/recipe/SimpleDatabase.scala
@@ -1,5 +1,17 @@
 package com.my.scala.chapter29.org.stairwaybook.recipe
 
+import com.my.scala.chapter29.org.stairwaybook.recipe.SimpleDatabase.FoodCategory
+
+
+abstract class Database {
+  def allFoods: List[Food]
+  def allRecipes: List[Recipe]
+  def foodNamed(name: String) =
+    allFoods.find(f => f.name == name)
+  case class FoodCategory(name: String, foods: List[Food])
+  def allCategories: List[FoodCategory]
+}
+
 object SimpleDatabase {
   def allFoods = List(Apple, Orange, Cream, Sugar)
   def foodNamed(name: String): Option[Food] =
@@ -13,10 +25,53 @@ object SimpleDatabase {
   def allCategories = categories
 }
 
+object SimpleDatabase2 extends Database {
+  def allFoods = List(Apple, Orange, Cream, Sugar)
+  def allRecipes: List[Recipe] = List(FruitSalad)
+  private var categories = List(
+    FoodCategory("fruits", List(Apple, Orange)),
+    FoodCategory("misc", List(Cream, Sugar))
+  )
+  def allCategories = categories
+}
+
+abstract class Browser {
+  val database: Database
+  def recipesUsing(food: Food) =
+    database.allRecipes.filter(recipe =>
+      recipe.ingredients.contains(food))
+
+  def displayCategory(category: database.FoodCategory): Unit = {
+    println(category)
+  }
+}
+
 object SimpleBrowser {
   def recipesUsing(food: Food) =
     SimpleDatabase.allRecipes.filter(recipe => recipe.ingredients.contains(food))
   def displayCategory(category: SimpleDatabase.FoodCategory) {
     println(category)
   }
+}
+
+object SimpleBrowser2 extends Browser {
+  val database = SimpleDatabase2
+}
+
+object StudentDatabase extends Database {
+  object FrozenFood extends Food("FrozenFood")
+  object HeatItUp extends Recipe(
+    "heat it up",
+    List(FrozenFood),
+    "Microwave the 'food' for 10 minutes"
+  )
+  def allFoods = List(FrozenFood)
+  def allRecipes = List(HeatItUp)
+  def allCategories = List(
+    FoodCategory("edible", List(FrozenFood))
+  )
+}
+
+object StudentBrowser extends Browser {
+  val database = StudentBrowser
 }
