9bb5e4a12fd37b5f553ab28f2d70a13903504e5d
Dmitry Palaznik <dpalaznik@gmail.com>
2015-12-03 10:51:47 +0300

    Modular Programming Using Objects: a recipe application

--- src/main/scala/com/my/scala/chapter29/ModularProgramming.scala
+++ src/main/scala/com/my/scala/chapter29/ModularProgramming.scala
@@ -1,5 +1,12 @@
 package com.my.scala.chapter29
 
+import com.my.scala.chapter29.org.stairwaybook.recipe.{SimpleBrowser, SimpleDatabase}
+
 object ModularProgramming {
 
+  def main(args: Array[String]) {
+    val apple = SimpleDatabase.foodNamed("Apple").get
+    println(apple)
+    println(SimpleBrowser.recipesUsing(apple))
+  }
 }
--- src/main/scala/com/my/scala/chapter29/org/stairwaybook/recipe/Recipe.scala
+++ src/main/scala/com/my/scala/chapter29/org/stairwaybook/recipe/Recipe.scala
@@ -1,5 +1,24 @@
 package com.my.scala.chapter29.org.stairwaybook.recipe
 
-class Recipe {
-
+abstract class Food(val name: String) {
+  override def toString = name
 }
+
+class Recipe (
+  val name: String,
+  val ingredients: List[Food],
+  val instructions: String
+) {
+  override def toString = name
+}
+
+object Apple extends Food("Apple")
+object Orange extends Food("Orange")
+object Cream extends Food("Cream")
+object Sugar extends Food("Sugar")
+
+object FruitSalad extends Recipe (
+  "fruit salad",
+  List(Apple, Orange, Cream, Sugar),
+  "Stir it all together."
+)
