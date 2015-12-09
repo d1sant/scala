2ec0026c38a08e21a4dab0d060dcaf0797794484
Dmitry Palaznik <dpalaznik@gmail.com>
2015-12-09 14:48:17 +0300

    Actors and Concurrency: actors and message passing

--- build.sbt
+++ build.sbt
@@ -2,7 +2,7 @@ name := "scala"
 
 version := "1.0"
 
-scalaVersion := "2.11.5"
+scalaVersion := "2.11.7"
 
 libraryDependencies += "org.scalatest" % "scalatest_2.11" % "2.2.4" % "test"
 
@@ -16,4 +16,10 @@ libraryDependencies += "org.scalacheck" %% "scalacheck" % "1.12.2" % "test"
 
 libraryDependencies += "org.scala-lang.modules" %% "scala-xml" % "1.0.2"
 
+// libraryDependencies += "com.typesafe.akka" %% "akka-actor" % "2.4-SNAPSHOT"
+
+libraryDependencies += "org.scala-lang" % "scala-actors" % "2.11.7"
+
 libraryDependencies ++= Seq("junit" % "junit" % "4.8.1" % "test")
+
+// resolvers += "Akka Snapshot Repository" at "http://repo.akka.io/snapshots/"
--- src/main/scala/com/my/scala/chapter32/Actors.scala
+++ src/main/scala/com/my/scala/chapter32/Actors.scala
@@ -1,5 +1,69 @@
 package com.my.scala.chapter32
 
-class Actors {
+object Actors {
 
+  def main(args: Array[String]) {
+
+    // println(SillyActor.start())
+    // Thread.sleep(10000)
+
+    println(SillyActor.start())
+    println(SeriousActor.start())
+
+    import scala.actors.Actor._
+    val seriousActor2 = actor {
+      for (i <- 1 to 5) {
+        println("That is the question")
+        Thread.sleep(1000)
+      }
+    }
+
+    println(SillyActor ! "hi there") // nothing happened
+
+    val echoActor = actor {
+      while (true) {
+        receive {
+          case msg =>
+            println("received message: " + msg)
+        }
+      }
+    }
+
+    echoActor ! "hi there"
+    echoActor ! "15"
+
+    val intActor = actor {
+      receive {
+        case x: Int => // I only want Ints
+          println("Got an Int: " + x)
+      }
+    }
+
+    intActor ! "hello" // will be ignored
+    intActor ! math.Pi // will be ignored
+
+    intActor ! 12
+  }
 }
+
+
+import scala.actors._
+
+object SillyActor extends Actor {
+  def act = {
+    for (i <- 1 to 5) {
+      println("I'm acting!")
+      Thread.sleep(1000)
+    }
+  }
+}
+
+object SeriousActor extends Actor {
+  def act = {
+    for (i <- 1 to 5) {
+      println("To be or not to be.")
+      Thread.sleep(1000)
+    }
+  }
+}
+
