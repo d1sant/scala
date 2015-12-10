5d8cb6acfbbf8d4e6edec0a06e78d366ec6943a3
Dmitry Palaznik <dpalaznik@gmail.com>
2015-12-10 18:28:26 +0300

    Actors and Concurrency: a longer example: Parallel discrete event simulation

--- src/main/scala/com/my/scala/chapter32/Simulation.scala
+++ src/main/scala/com/my/scala/chapter32/Simulation.scala
@@ -5,6 +5,24 @@ import scala.actors.Actor._
 
 object Simulation {
 
+  // TODO Make it work!!! )
+
+  def main(args: Array[String]) {
+    val circuit = new Circuit with Adders
+    import circuit._
+    val ain = new Wire("ain", true)
+    val bin = new Wire("bin", false)
+    val cin = new Wire("cin", true)
+    val sout = new Wire("sout")
+    val cout = new Wire("cout")
+    prob(ain)
+    prob(bin)
+    prob(cin)
+    prob(sout)
+    prob(cout)
+    fullAdder(ain, bin, cin, sout, cout)
+    circuit.start()
+  }
 }
 
 trait Simulant extends Actor {
@@ -24,7 +42,114 @@ trait Simulant extends Actor {
   }
   start()
 }
-abstract class Wire extends Simulant
+
+class Circuit {
+
+  val clock = new Clock
+
+  // simulation messages
+  case class SetSignal(sig: Boolean)
+  case class SignalChanged(wire: Wire, sig: Boolean)
+
+  // delay constants
+  val WireDelay = 1
+  val InverterDelay = 2
+  val OrGateDelay = 3
+  val AndGateDelay = 3
+
+  // Wire and Gate classes and methods
+  class Wire(name: String, init: Boolean) extends Simulant {
+    def this(name: String) { this(name, false) }
+    def this() { this("unnamed") }
+    val clock = Circuit.this.clock
+    clock.add(this)
+    private var sigVal = init
+    private var observers: List[Actor] = List()
+    def handleSimMessage(msg: Any) {
+      msg match {
+        case SetSignal(s) =>
+          if (s != sigVal) {
+            sigVal = s
+            signalObservers()
+          }
+      }
+    }
+    def signalObservers() {
+      for (obs <- observers)
+        clock ! AfterDelay(WireDelay, SignalChanged(this, sigVal), obs)
+    }
+    override def simStarting() { signalObservers() }
+    def addObservers(obs: Actor) {
+      observers = obs :: observers
+    }
+    override def toString = "Wire(" + name + ")"
+  }
+  private object DummyWire extends Wire("dummy")
+  abstract class Gate(in1: Wire, in2: Wire, out: Wire) extends Simulant {
+    def computeOutput(s1: Boolean, s2: Boolean): Boolean
+    val delay: Int
+    val clock = Circuit.this.clock
+    clock.add(this)
+    in1.addObservers(this)
+    in2.addObservers(this)
+    var s1, s2 = false
+    override def handleSimMessage(msg: Any) {
+      msg match {
+        case SignalChanged(w, sig) =>
+          if (w == in1)
+            s1 = sig
+          if (w == in2)
+            s2 = sig
+          clock ! AfterDelay(delay, SetSignal(computeOutput(s1, s2)), out)
+      }
+    }
+  }
+
+  // misc. utility methods
+  def orGate(in1: Wire, in2: Wire, output: Wire) =
+    new Gate(in1, in2, output) {
+      val delay = OrGateDelay
+      def computeOutput(s1: Boolean, s2: Boolean) = s1 || s2
+    }
+  def andGate(in1: Wire, in2: Wire, output: Wire) =
+    new Gate(in1, in2, output) {
+      val delay = AndGateDelay
+      def computeOutput(s1: Boolean, s2: Boolean) = s1 && s2
+    }
+  def inverter(input: Wire, output: Wire) =
+    new Gate(input, DummyWire, output) {
+      val delay = InverterDelay
+      def computeOutput(s1: Boolean, ignored: Boolean) = !s1
+    }
+  def prob(wire: Wire) = new Simulant {
+    val clock = Circuit.this.clock
+    clock.add(this)
+    wire.addObservers(this)
+    def handleSimMessage(msg: Any): Unit = {
+      msg match {
+        case SignalChanged(w, s) =>
+          println("signal " + w + " changed to " + s)
+      }
+    }
+  }
+  def start() { clock ! Start }
+}
+
+trait Adders extends Circuit {
+  def halfAdder(a: Wire, b: Wire, s: Wire, c: Wire) {
+    val d, e = new Wire
+    orGate(a, b, d)
+    andGate(a, b, c)
+    inverter(c, e)
+    andGate(d, e, s)
+  }
+  def fullAdder(a: Wire, b: Wire, cin: Wire, sum: Wire, cout: Wire) {
+    val s, c1, c2 = new Wire
+    halfAdder(a, cin, s, c1)
+    halfAdder(b, s, sum, c2)
+    orGate(c1, c2, cout)
+  }
+}
 
 case class Ping(time: Int)
 case class Pong(time: Int, from: Actor)
