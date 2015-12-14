2b5fcc74a06bc5f4c360fa303e708c1c2ec736b5
Dmitry Palaznik <dpalaznik@gmail.com>
2015-12-14 11:32:03 +0300

    Combinator parsing: running your parser

--- src/main/scala/com/my/scala/chapter33/Arith.scala
+++ src/main/scala/com/my/scala/chapter33/Arith.scala
@@ -6,3 +6,20 @@ class Arith extends JavaTokenParsers {
   def term: Parser[Any] = factor~rep("*"~factor | "/"~factor)
   def factor: Parser[Any] = floatingPointNumber | "("~expr~")"
 }
+
+object ParseExpr extends Arith {
+
+  def main(args: Array[String]) {
+
+    // println("input : " + args(0))
+    // println(parseAll(expr, args(0)))
+
+    val input = "2 * (3 + 7)"
+    println("input : " + input)
+    println(parseAll(expr, input))
+
+    val input2 = "2 * (3 + 7))"
+    println("input : " + input2)
+    println(parseAll(expr, input2)) // produces an error: failure: string matching regex
+  }
+}
